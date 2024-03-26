a979f2122914acd1b8f5db09da9d1bf9777c0296
LJ1319 <luka.jikia.2@btu.edu.ge>
2024-03-26 18:46:12 +0400

    refactor: define global validators using function

---
+++ src/utils/defineGlobalValidators.js
@@ -0,0 +1,35 @@
+import { defineRule } from 'vee-validate'
+import { emailRegex } from '@/config/regex.js'
+
+export default function defineGlobalValidators() {
+  defineRule('required', (value) => {
+    if (!value) {
+      return 'This field is required'
+    }
+
+    return true
+  })
+
+  defineRule('min', (value, [limit]) => {
+    if (value.length < limit) {
+      return `This field must be at least ${limit} characters`
+    }
+
+    return true
+  })
+
+  defineRule('email', (value) => {
+    if (!emailRegex.test(value)) {
+      return 'This field must be a valid email'
+    }
+
+    return true
+  })
+
+  defineRule('confirmed', (value, [target], ctx) => {
+    if (value === ctx.form[target]) {
+      return true
+    }
+    return 'Passwords must match'
+  })
+}
